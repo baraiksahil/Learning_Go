e572b0a44cda048656b2cd5082c3fb3ce6c8d86f
Baraik Sahil Kumar <baraiksahilkumar@gmail.com>
2026-02-20 00:27:34 +0530

    Lean about basic printing, and simple values

---
+++ 02_simple_values/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// int
+	fmt.Println(1);
+
+	// boolean
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// float
+	fmt.Println(10.32)
+
+	// string
+	fmt.Println("Hello golang")
+}
